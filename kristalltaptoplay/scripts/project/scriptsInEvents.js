cd9e0361271d92e74a2b2bf3bd8e2e3743be904d
TAP777PLAY <156446211+TAP777PLAY@users.noreply.github.com>
2024-05-08 18:28:43 +0300

    Add files via upload

---
+++ kristalltaptoplay/scripts/project/scriptsInEvents.js
@@ -0,0 +1,46 @@
+
+
+
+const scriptsInEvents = {
+
+	async GameEvents_Event2_Act1(runtime, localVars)
+	{
+		localVars.variantName1 = new Date (runtime.GameScore.serverTime).toLocaleString('RU', {
+		 day: '2-digit',
+		  month: '2-digit',
+		  weekday: 'long'
+		  
+		   
+		});
+	},
+
+	async GameEvents_Event2_Act2(runtime, localVars)
+	{
+		localVars.month1 = new Date (runtime.GameScore.serverTime).toLocaleString('RU', {
+		  month: '2-digit',
+		  year: 'numeric',
+		   
+		   
+		});
+	},
+
+	async GameEvents_Event2_Act3(runtime, localVars)
+	{
+		let date = new Date();
+		date.setDate(date.getDate() - 1);
+		localVars.lastDate1 = date.toLocaleString('ru', {
+		 day: '2-digit',
+		  month: '2-digit',
+		  weekday: 'long'
+		});
+	},
+
+	async MenuEvents_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#9bb598'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
